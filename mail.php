673cf016ab148e38d33009dae7f0b261c80625ab
IvanSeagull <47693465+IvanSeagull@users.noreply.github.com>
2019-08-03 19:11:40 +0300

    2

--- mail.php
+++ mail.php
@@ -16,7 +16,7 @@ $mail->SMTPAuth = true;                               // Enable SMTP authenticat
 $mail->Username = 'ivan.seagull.k@gmail.com'; // Ваш логин от почты с которой будут отправляться письма
 $mail->Password = 'qqq2qqq2'; // Ваш пароль от почты с которой будут отправляться письма
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
-$mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
+$mail->Port = 25; // TCP port to connect to / этот порт может отличаться у других провайдеров
 
 $mail->setFrom('ivan.seagull.k@gmail.com'); // от кого будет уходить письмо?
 $mail->addAddress('ivan.playchanel.k@gmail.com');     // Кому будет уходить письмо
